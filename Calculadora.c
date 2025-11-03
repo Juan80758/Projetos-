48fe667537784ca5c441850bd45553480afeb7ab
Juan80758 <juanvictorsantana456@gmail.com>
2025-11-03 18:21:34 -0300

    Versão com multiplicação e divisão

--- Calculadora.c
+++ Calculadora.c
@@ -18,6 +18,13 @@ int main(){
         resultado = num1 - num2;
     } else if(operador == '*') {
         resultado = num1 * num2;
+    } else if(operador == '/') {
+        if(num2 == 0) {
+            printf("Numero invalido, por favor coloque outro numero \n");
+            return 1;
+        } else {
+            resultado = num1 / num2;
+        }
     }
     printf("O resultado foi: %.2f \n", resultado);
 
