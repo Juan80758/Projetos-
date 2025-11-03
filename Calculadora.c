47ca675127bc63ec0ec8cd4509189a686b8e2a21
Juan80758 <juanvictorsantana456@gmail.com>
2025-11-03 11:08:58 -0300

    Primeira versão de uma calculadora em C

---
+++ Calculadora.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main(){
+    float num1, num2, resultado;
+    char operador;
+
+    printf("Digite o primeiro numero aqui: \n");
+    scanf(" %f", &num1);
+
+    printf("Digite agora o operador: \n");
+    scanf(" %c", &operador);
+
+    printf("Digite o segundo numero agora: \n");
+    scanf(" %f", &num2);
+
+    if(operador == '+') {
+        resultado = num1 + num2;
+    } else if(operador == '-') {
+        resultado = num1 - num2;
+    }
+    printf("O resultado foi: %.2f \n", resultado);
+    return 0;
+}
